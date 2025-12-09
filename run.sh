439f3e26e34498fc90c9711f05cf81ff649ce9e1
torrid-fish <justinwu6900942@gmail.com>
2025-12-10 00:30:42 +0800

    refactor: rename files

--- run.sh
+++ run.sh
@@ -4,33 +4,33 @@
 
 # 1 has been evaluated already
 for template_id in 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_scannet200/adversarial_template_${template_id}.json
 done
 
 for template_id in 1 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_scannet200/popular_template_${template_id}.json
 done
 
 for template_id in 1 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_scannet200/random_template_${template_id}.json
 done
 
 ############## Nyu40 Evaluation #################
 
 for template_id in 1 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_nyu40/adversarial_template_${template_id}.json
 done
 
 for template_id in 1 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_nyu40/popular_template_${template_id}.json
 done
 
 for template_id in 1 2 3 4; do
-    python run_eval_leo.py \
+    python eval_leo.py \
         scannet_nyu40/random_template_${template_id}.json
 done
