10025fa5cb4358ac638f3787e98d70636b963ba6
torrid-fish <justinwu6900942@gmail.com>
2025-12-09 22:20:54 +0800

    feat: ensure LEO inference working under original folder

---
+++ eval_leo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python launch.py --mode python \
+                 --run_file inference.py \
+                 --config configs/default.yaml \
+                 note=tuning_noact \
+                 pretrained_ckpt_path=./sft_noact/ \
+                 data.scan_family_base=../
