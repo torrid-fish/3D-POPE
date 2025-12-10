f2e67aa643a4b467600a97e6d50dd0bc15a8b62b
torrid-fish <justinwu6900942@gmail.com>
2025-12-10 11:50:50 +0800

    chore: update run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,25 @@
 
 ########### Scannet200 Evaluation ###############
 
-# 1 has been evaluated already
-for template_id in 2 3 4; do
+for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_scannet200/adversarial_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: scannet200_adversarial_template_${template_id} complete"
 done
 
 for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_scannet200/popular_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: scannet200_popular_template_${template_id}"
 done
 
 for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_scannet200/random_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: scannet200_random_template_${template_id} complete"
 done
 
 ############## Nyu40 Evaluation #################
@@ -23,14 +28,20 @@ done
 for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_nyu40/adversarial_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: nyu40_adversarial_template_${template_id} complete"
 done
 
 for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_nyu40/popular_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: nyu40_popular_template_${template_id} complete"
 done
 
 for template_id in 1 2 3 4; do
     python eval_leo.py \
         scannet_nyu40/random_template_${template_id}.json
+    git add eval_results/
+    git commit -m "chore: nyu40_random_template_${template_id} complete"
 done
